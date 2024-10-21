b078e00d0eef468a6b55116733ad5aa0a0f55314
Elton Chan <61935006+eltonhcchan@users.noreply.github.com>
2024-10-21 15:59:23 +0000

    move file to github

--- python/tests/test01.py
+++ python/tests/test01.py
@@ -5,8 +5,16 @@ ad = Address('禮頓道33號')
 j = ad._result
 msg = "Hello"
 print(msg)
-print(json.dumps(j, indent=4))
-print(j[1]['geo']['Northing'])
+json_string = json.dumps(j, indent=4, ensure_ascii=False).encode('utf8')
+print(json_string.decode())
+print(j[0]['geo']['Northing'])
 print(msg)
 #result = ad.ParseAddress()
 #print(result)
+
+x = ad._inputAddr
+print(x)
+
+from json2table import convert
+html=convert(json_string)
+print(html)
